9e9ac1f575ed3f9c4426af6bdd57ee0c646acaa9
roman-jordan <rjordan1352@gmail.com>
2020-06-15 20:03:36 -0700

    Mark Down Parser Class Seperated from index.php

--- PHP/markdown-parser.php
+++ PHP/markdown-parser.php
@@ -1,20 +1,56 @@
-<?php 
-
+<?php
 class MarkDownParser
 {
-    private function __construct($string)
+    protected $string,
+              $error;
+
+    public function __construct($string)
     {
-        if (isset($string)) {
-            return 'hello';
+        if (is_string($string)) {
+            $string = $this->normalize_eol($string);
+            $this->string = $this->string_to_array($string);
         }
+    }
+
+    private function normalize_eol($string)
+    {
+        return str_replace(["\r\n", "\r"], "\n", $string);
+    }
+
+    private function string_to_array($string)
+    {
+        return explode("\n", $string);
+    }
+
+    private function rule_headings($string)
+    {
+        if (strpos($string, '#') === 0) {
+
+            $hash = explode(' ', $string, 2);
+            $count = substr_count($hash[0], '#');
+            if ($count === strlen($hash[0])) {
+                return '<h' . $count . '>' . $hash[1] . '</h' . $count . '>';
+            }
+            return $string;
+        };
+        return $string;
     }
 
     public function toHTML()
     {
-        
+        foreach (get_class_methods($this) as $rule) {
+            if (!isset($this->error)) {
+                if (strpos($rule, 'rule_') !== false) {
+                    foreach ($this->string as $subString) {
+                        //This should be done in parallel 
+                        $this->string[$subString] = $this->$rule($subString);
+                        echo $subString;
+                    }
+                }
+            }
+        };
+
+        $this->string = implode("\n", $this->string);
+        return $this->string;
     }
 }
-
-$md = new MarkDownParser('hello');
-
-echo $md;
